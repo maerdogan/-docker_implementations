36f285a2d3232140ac46ee619f2248497d2d6983
maerdogan <56476441+maerdogan@users.noreply.github.com>
2021-12-26 13:59:29 +0300

    Create create_domain.py

---
+++ fmw/weblogic/12214/wls_scripts/create_domain.py
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+mkdir -p /u01/oracle/domains/denemeDomain1/servers/AdminServer/security
+echo "username=weblogic" >> /u01/oracle/domains/denemeDomain1/servers/AdminServer/security/boot.properties
+echo "password=welcome1" >> /u01/oracle/domains/denemeDomain1/servers/AdminServer/security/boot.properties
+
+nohup /u01/oracle/domains/denemeDomain1/bin/startWebLogic.sh &
+nohup /u01/oracle/domains/denemeDomain1/bin/startNodeManager.sh &
+
+sleep infinity 
